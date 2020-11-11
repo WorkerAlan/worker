ad1cc7ce42a61600f1cbc119073d1c3c1f2c0ecd
ningzhiqiang <ningzhiqiang@zhuanzhuan.com>
2020-11-11 23:25:50 +0800

    扩展 string

--- Alex/Kit/String+Extensions.swift
+++ Alex/Kit/String+Extensions.swift
@@ -6,10 +6,20 @@
 //
 
 import Foundation
+import UIKit
+import CoreText
 
 extension String {
     var oc_string: NSString {
         return self as NSString
+    }
+    
+    var doubleValue: Double {
+        return oc_string.doubleValue
+    }
+    
+    var floatValue: Float {
+        return oc_string.floatValue
     }
     
     var intValue: Int32 {
@@ -46,4 +56,59 @@ extension String {
     var hexInt: Int {
         return Int(self, radix: 16) ?? 0
     }
+    
+    var toTrim: String {
+        return trimmingCharacters(in: CharacterSet.whitespaces)
+    }
+    
+    var percentEncoding: String? {
+        return addingPercentEncoding(withAllowedCharacters: CharacterSet.urlHostAllowed)
+    }
+    
+    var toURL: URL? {
+        return URL(string: percentEncoding ?? "")
+    }
+    
+    func rectSize(_ font: UIFont, _ size: CGSize) -> CGSize {
+        return oc_string.boundingRect(with: size, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [.font: font], context: nil).size
+    }
+    
+    func rectHeight(_ font: UIFont, _ size: CGSize) -> CGFloat {
+        return rectSize(font, size).height
+    }
+    
+    func rectWidth(_ font: UIFont, _ size: CGSize) -> CGFloat {
+        return rectSize(font, size).width
+    }
+    
+    func singlineSize(_ font: UIFont) -> CGSize {
+        return oc_string.size(withAttributes: [.font: font])
+    }
+    
+    func singlineHeight(_ font: UIFont) -> CGFloat {
+        return singlineSize(font).height
+    }
+    
+    func singlineWidth(_ font: UIFont) -> CGFloat {
+        return singlineSize(font).width
+    }
+    
+    fileprivate func validIndex(_ index: Int) -> Int {
+        return 0
+    }
+    
+    func substring(to index: Int) -> String {
+        return oc_string.substring(to: index)
+    }
+    
+    func substring(from index: Int) -> String {
+        return oc_string.substring(from: index)
+    }
+    
+    func substring(with range: NSRange) -> String {
+        return oc_string.substring(with: range)
+    }
+//    func linesWithRect(_ font: UIFont, _ size: CGSize) -> Int {
+//
+//    }
 }
